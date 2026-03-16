81b911785eb33a48084ea98fa0977df9fef1991c
Tomas Tvaroska <tomas@tvaroska.com>
2026-03-16 17:58:01 +0000

    empty

--- src/main/java/frc/lib/drivetrain/DriveTelemetry.java
+++
@@ -1,9 +0,0 @@
-package frc.lib.drivetrain;
-
-/** Receives drivetrain state snapshots each cycle for dashboard publishing. */
-@FunctionalInterface
-public interface DriveTelemetry {
-
-    /** Called every cycle with the current drivetrain state. */
-    void update(DriveState state);
-}
